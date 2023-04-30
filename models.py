f69d6468eec7a277ebcbaac6fd477291214b56ff
Victor Torres <Zorro4@LIVE.COM.MX>
2023-04-29 18:16:56 -0700

    Creado database.py y models.py
    Implementado solo el modelo referente al login.

---
+++ models.py
@@ -0,0 +1,34 @@
+from sqlalchemy import ForeignKey, UniqueConstraint
+from sqlalchemy import Integer, String, DateTime, BINARY
+from sqlalchemy.orm import mapped_column, Mapped, relationship
+from sqlalchemy.dialects.mysql import TINYINT
+from database import Model
+
+class UserAccount(Model):
+    id: Mapped[int] = mapped_column('account_id', Integer, primary_key=True)
+    creation_time: Mapped[str] = mapped_column(DateTime, nullable=False)
+    status: Mapped[int] = mapped_column(TINYINT, nullable=False)
+    email: Mapped[str] = mapped_column(String(40), nullable=False)
+    username: Mapped[str] = mapped_column(String(24), nullable=False)
+    nombre: Mapped[str] = mapped_column(String(32), nullable=False)
+    apellido_p: Mapped[str] = mapped_column(String(24))
+    apellido_m: Mapped[str] = mapped_column(String(24))
+
+    login_info: Mapped['UserLoginInfo'] = relationship(back_populates='account')
+    logged_sessions: Mapped[list['UserSession']] = relationship(back_populates='account')
+
+class UserLoginInfo(Model):
+    account_id: Mapped[int] = mapped_column(ForeignKey('UserAccount.account_id'),
+                                    primary_key=True)
+    passwd_hash: Mapped[bytes] = mapped_column(String(128), nullable=False)
+
+    account: Mapped['UserAccount'] = relationship(back_populates='login_info')
+
+class UserSession(Model):
+    id: Mapped[bytes] = mapped_column('session_id', BINARY(44), primary_key=True)
+    session_id: Mapped[int] = mapped_column(ForeignKey('UserAccount.account_id'),
+                                            unique=True)
+
+    account: Mapped['UserAccount'] = relationship(back_populates='logged_sessions')
+
+
--- schema.sql
+++ schema.sql
@@ -1,9 +1,38 @@
 
-CREATE TABLE CSRF_TOKEN (
-	token CHAR(64) PRIMARY KEY
+CREATE TABLE CSRF_Token (
+	csrf_token CHAR(64) PRIMARY KEY
 );
 
-CREATE TABLE USER_SESSION (
+
+CREATE TABLE UserAccount (
+	account_id    INT NOT NULL;
+	creation_time Datetime NOT NULL;
+	status        TINYINT NOT NULL;
+	email         VARCHAR(40) NOT NULL;
+	username      VARCHAR(24) NOT NULL;
+	nombre        VARCHAR(24) NOT NULL;
+	apellido_p    VARCHAR(24);
+	apellido_m    VARCHAR(24);
+
+	CONSTRAINT PRIMARY KEY(account_id);
+);
+
+CREATE TABLE UserLogin (
+	account_id INT NOT NULL;
+	passwd_hash BINARY() NOT NULL;
+	passwd_salt BINARY() NOT NULL;
+	CONSTRAINT PRIMARY KEY(account_id);
+	CONSTRAINT FK_UserLogin_User FOREIGN KEY (account_id)
+	REFERENCES UserAccount(account_id);
+);
+
+
+CREATE TABLE UserSession (
 	session_id CHAR(44) PRIMARY KEY;
+	account_id INT NOT NULL;
+
+	CONSTRAINT FK_UserSession_UserAccount FOREIGN KEY (user_account)
+	REFERENCES UserAccount(user_account_id)
+	ON DELETE CASCADE;
 );
 
